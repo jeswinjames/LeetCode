01f199e125d3c3b5e009272809a8b9243980ffd5
Jeswin Prince <jeswin_prince@trimble.com>
2020-04-14 18:47:43 +0530

    LC27 Optimised

--- src/main/java/list/LC27.java
+++ src/main/java/list/LC27.java
@@ -2,24 +2,20 @@ package list;
 
 public class LC27 {
 
-    private LC27(){}
+    private LC27() {
+    }
 
     public static int removeElement(int[] nums, int val) {
         int leadingPointer = 0;
         int laggingPointer = 0;
-        int counter = 0;
         while (leadingPointer < nums.length) {
-            if (nums[leadingPointer] == val) {
-                leadingPointer++;
-            } else {
-                if (leadingPointer != laggingPointer) {
-                    nums[laggingPointer] = nums[leadingPointer];
-                }
+            if (nums[leadingPointer] != val) {
+                nums[laggingPointer] = nums[leadingPointer];
                 laggingPointer++;
-                leadingPointer++;
-                counter++;
             }
+            leadingPointer++;
         }
-        return counter;
+        return laggingPointer;
     }
 }
+
